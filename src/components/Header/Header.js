553a017e760486cff39d6e63c196c587d2e6eb7c
mickaelchoidev <mickaelchoidev@gmail.com>
2020-07-24 00:10:15 +0200

    padding link to adapt the mobile format

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,7 +24,7 @@ const Header = () => {
       transition={elementTransition}
       className="Header"
     >
-      <nav className="navbar navbar-expand-lg navbar-light bg-light py-2 px-3">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light py-0 pl-0 pr-4">
         <Link className="navbar-brand Header-logo" to="/">
           <img src={require("../../images/leadUpLogo.jpg")} alt="leadUp" />
         </Link>
@@ -41,35 +41,47 @@ const Header = () => {
         </button>
 
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ml-auto mt-2 mt-lg-0">
+          <ul className="navbar-nav ml-auto">
             <li className="nav-item">
-              <Link className="nav-link" to="/">
+              <Link className="nav-link px-4 px-lg-2 pb-4 py-lg-2" to="/">
                 Why Lead Up ?
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/who-are-we">
+              <Link
+                className="nav-link px-4 px-lg-2 pb-4 py-lg-2"
+                to="/who-are-we"
+              >
                 Who are we ?
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/our-know-how">
+              <Link
+                className="nav-link px-4 px-lg-2 pb-4 py-lg-2"
+                to="/our-know-how"
+              >
                 Our know-how
               </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/tuc">
+              <Link className="nav-link px-4 px-lg-2 pb-4 py-lg-2" to="/tuc">
                 TUC
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/clients-partners">
+              <Link
+                className="nav-link px-4 px-lg-2 pb-4 py-lg-2"
+                to="/clients-partners"
+              >
                 Clients and partners
               </Link>
             </li>
             <li className="nav-item">
-              <Link className="nav-link" to="/contact">
+              <Link
+                className="nav-link px-4 px-lg-2 pb-4 py-lg-2"
+                to="/contact"
+              >
                 Contact us
               </Link>
             </li>
